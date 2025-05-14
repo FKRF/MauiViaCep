46344576add4c688c9ca4fc063ca82b10efe2820
FKRF <153143103+FKRF@users.noreply.github.com>
2025-05-13 23:08:22 -0300

    Add project files.

---
+++ MauiViaCep/Endereco.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace MauiViaCep
+{
+    internal class Endereco
+    {
+        public string Cep { get; set; }
+        [JsonProperty("logradouro")]
+        public string Logradouro { get; set; }
+        public string Complemento { get; set; }
+        public string Unidade { get; set; }
+        public string Bairro { get; set; }
+        public string Localidade { get; set; }
+        public string Uf { get; set; }
+        public string Estado { get; set; }
+        public string Regiao { get; set; }
+        public int? Ibge { get; set; }
+        public int? Gia { get; set; }
+        public int? Ddd { get; set; }
+        public int? Siafi { get; set; }
+    }
+}
---
+++ MauiViaCep/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace MauiViaCep
+{
+    public partial class MainPage : ContentPage
+    {
+        private const string VIA_CEP_URL = "http://viacep.com.br/ws/";
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnConsultarClicked(object sender, EventArgs e)
+        {
+            string cep = cepEntry.Text.Trim().Replace("-", "");
+            if (string.IsNullOrWhiteSpace(cep) || cep.Length != 8 || (cep) )
+            {
+                await DisplayAlert("Erro", "Digite um CEP!", "Ok");
+                return;
+            }
+            try
+            {
+                using var client = new HttpClient();
+                string url = $"{VIA_CEP_URL}/{cep}/json";
+
+                var response = await client.GetStringAsync(url);
+                if (response.Contains("\"erro\""))
+                    resultadoLbl.Text = "CEP não encontrado!";
+                    //Console.WriteLine()
+                else
+                {
+                    var endereco = JsonConvert.DeserializeObject<Endereco>(response);
+                    resultadoLbl.Text = $"Logradouro: {endereco.Logradouro}\n"
+                                        + $"Numero: {endereco.Unidade}\n"
+                                        + $"Bairro: {endereco.Bairro}\n"
+                                        + $"Cidade: {endereco.Localidade}\n"
+                                        + $"Bairro: {endereco.Uf}\n";
+                    
+                }
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Erro", $"Erro ao consultar cep {cep}: {ex.Message}", "Ok");
+            }
+        }
+    }
+
+}
